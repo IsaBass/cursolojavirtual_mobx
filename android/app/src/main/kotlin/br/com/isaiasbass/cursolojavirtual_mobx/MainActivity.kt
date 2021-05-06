f71e982a977d369d320c4c5116f4a526d6e79a36
Isaias MacOS <isaiasgoncalves.igs@gmail.com>
2021-05-06 18:34:42 -0300

    habilitado para web

---
+++ android/app/src/main/kotlin/br/com/isaiasbass/cursolojavirtual_mobx/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.isaiasbass.cursolojavirtual_mobx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ web/init-firebase.js
@@ -0,0 +1,24 @@
+
+function doSomething()  {
+
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyCxOKyZIz7bWddPz5d2syRgZmhRiFGjzgE",
+        authDomain: "cursolojavirtual-c55f4.firebaseapp.com",
+        databaseURL: "https://cursolojavirtual-c55f4.firebaseio.com",
+        projectId: "cursolojavirtual-c55f4",
+        storageBucket: "cursolojavirtual-c55f4.appspot.com",
+        messagingSenderId: "876900207335",
+        appId: "1:876900207335:web:e4d9c516be9c1afb4cf042",
+        measurementId: "G-WHQQY6TFX9"
+      };
+
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
+    }
+
+    doSomething();
+
+
